8e2db077f9c9feaa188fbc07112df2d7b4d3632f
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2024-03-09 15:40:00 +0100

    migration to ansible from cloud-init

--- lab2/terraform/gcp/main.tf
+++ lab2/terraform/gcp/main.tf
@@ -4,10 +4,11 @@ module "compute-instance" {
   credentials = var.credentials
   project-id  = var.project-id
 
-  name            = "pcpc-lab2"
-  ssh-user        = "root"
-  machines-count  = 2
-  cloud-init-file = "cloud-init.yaml"
+  name             = "pcpc-lab2"
+  ssh-user         = "root"
+  ssh-pk-save-path = var.ssh-pk-save-path
+  machines-count   = 2
+  cloud-init-file  = "cloud-init.yaml"
   # run "cloud-init status --wait" in the SSH to check when it is done
   # run "tail -f /var/log/cloud-init-output.log" to see what it is doing
 
--- lab2/terraform/gcp/variables.tf
+++ lab2/terraform/gcp/variables.tf
@@ -7,3 +7,7 @@ variable "project-id" {
   type      = string
   sensitive = true
 }
+
+variable "ssh-pk-save-path" {
+  type = string
+}
