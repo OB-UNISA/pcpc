a0525bc2dac900c4d6c8c6e9051a199a3aa7d4da
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2024-04-13 17:55:07 +0200

    lab5_4

--- labs/mycollective/mycollective.h
+++ labs/mycollective/mycollective.h
@@ -3,10 +3,22 @@
 
 void broadcast(int P, int rank, void *buf, int count, MPI_Datatype data_type, MPI_Comm comm);
 
+void i_broadcast(int P, int rank, void *buf, int count, MPI_Datatype data_type, MPI_Comm comm);
+
 void gather(int P, int rank, void *buf, int count, MPI_Datatype data_type, MPI_Comm comm);
+
+void i_gather(int P, int rank, void *buf, int count, MPI_Datatype data_type, MPI_Comm comm);
 
 int scatter(int P, int rank, void *buf, int count, MPI_Datatype data_type, MPI_Comm comm);
 
+int i_scatter(int P, int rank, void *buf, int count, MPI_Datatype data_type, MPI_Comm comm);
+
 int min(int P, int rank, int *array, int count, MPI_Comm comm);
 
+int i_min(int P, int rank, int *array, int count, MPI_Comm comm);
+
 int max(int P, int rank, int *array, int count, MPI_Comm comm);
+
+int i_max(int P, int rank, int *array, int count, MPI_Comm comm);
+
+void wait_all(MPI_Request requests[], int count);
